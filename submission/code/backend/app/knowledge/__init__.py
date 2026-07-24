785bd3d691522e55bcedf836447fd40fbc82835d
nh-atuan <anhtuan21347@gmail.com>
2026-07-24 17:17:55 +0700

    chore(git): ignore and untrack report/, slides/, submission/, and docs/

--- submission/code/backend/app/knowledge/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Expert knowledge base package."""
-
-from backend.app.knowledge.knowledge_base import KnowledgeBase
-
-__all__ = ["KnowledgeBase"]
--- submission/code/src/segmentation/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Optional SAM 3 segmentation module."""
-
-from src.segmentation.sam3_segmenter import SAM3Segmenter
-
-__all__ = ["SAM3Segmenter"]
